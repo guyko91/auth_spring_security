53ab433a89c0241402bd67890d9148fd9676f976
larry <guyko91@gmail.com>
2024-12-02 17:56:50 +0900

    [chore] 네이버 연동 삭제 (시연용)

--- src/main/java/com/pineone/auth/security/oauth/OAuth2Provider.java
+++ src/main/java/com/pineone/auth/security/oauth/OAuth2Provider.java
@@ -10,8 +10,7 @@ public enum OAuth2Provider {
 
     GOOGLE(null, "sub"),
     FACEBOOK(null, "id"),
-    KAKAO("kakao_account", "id"),
-    NAVER("response", "id")
+    KAKAO("kakao_account", "id")
     ;
 
     // OAuth 서버로 부터 받은 JSON 데이터 중 사용자 정보를 파싱하기 위한 키값 (null 인 경우 1depth)
